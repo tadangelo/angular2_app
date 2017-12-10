cc24cf5cf0ddddaa601193e089a86a9563f62a76
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-10 05:17:21 -0600

    Added bootstrap card for document components

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -16,21 +16,21 @@ var DocumentsComponent = /** @class */ (function () {
                 description: "test test test",
                 file_url: "http://google.com",
                 updated_at: "12/8/17",
-                image_url: "http://google.com"
+                image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
             },
             {
                 title: "My Second Doc",
                 description: "test test test",
                 file_url: "http://google.com",
                 updated_at: "12/8/17",
-                image_url: "http://google.com"
+                image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
             },
             {
                 title: "My Last Doc",
                 description: "test test test",
                 file_url: "http://google.com",
                 updated_at: "12/8/17",
-                image_url: "http://google.com"
+                image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
             }
         ];
     }
