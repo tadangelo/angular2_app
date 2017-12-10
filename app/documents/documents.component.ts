54631ebf4ba5decfa47edbfa6b21147191551bce
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-09 19:17:13 -0600

    Created mock data for angular app

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
@@ -7,5 +8,27 @@ import { Component } from '@angular/core'
 })
 
 export class DocumentsComponent {
-  
+  documents: Document[] = [
+    {
+      title: "My First Doc",
+      description: "test test test",
+      file_url: "http://google.com",
+      updated_at: "12/8/17",
+      image_url: "http://google.com"
+    },
+    {
+      title: "My Second Doc",
+      description: "test test test",
+      file_url: "http://google.com",
+      updated_at: "12/8/17",
+      image_url: "http://google.com"
+    },
+    {
+      title: "My Last Doc",
+      description: "test test test",
+      file_url: "http://google.com",
+      updated_at: "12/8/17",
+      image_url: "http://google.com"
+    }
+  ]
 }
