d60c7f307e5245b070f3956977a230390cd44b6e
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-09 23:27:44 -0600

    Added  proposals link to views

---
+++ app/proposal/proposal-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selctor: 'proposal-list',
+  templateUrl: 'proposal-list.component.html'
+})
+export class ProposalListComponent {}
