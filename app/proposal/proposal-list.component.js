7ab126f808129e38a8a809592f70ffc7edb9e3e6
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-10 00:05:14 -0600

    Created list view data for proposals

--- app/proposal/proposal-list.component.js
+++ app/proposal/proposal-list.component.js
@@ -7,8 +7,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var proposal_1 = require("./proposal");
 var ProposalListComponent = /** @class */ (function () {
     function ProposalListComponent() {
+        this.proposalOne = new proposal_1.Proposal(15, 'AJE Company', 'http://portfolio.taniadangelo.com', 'Ruby on Rails', 150, 120, 15, 'test@test.com');
+        this.proposalTwo = new proposal_1.Proposal(99, 'JEA Company', 'http://portfolio.taniadangelo.com', 'Ruby on Rails', 150, 120, 15, 'test@test.com');
+        this.proposalThree = new proposal_1.Proposal(300, 'EAJ Company', 'http://portfolio.taniadangelo.com', 'Ruby on Rails', 150, 120, 15, 'test@test.com');
+        this.proposals = [
+            this.proposalOne,
+            this.proposalTwo,
+            this.proposalThree
+        ];
     }
     ProposalListComponent = __decorate([
         core_1.Component({
--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
   selctor: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(15, 'AJE Company', 'http://portfolio.taniadangelo.com', 'Ruby on Rails', 150, 120, 15, 'test@test.com')
+  proposalTwo: Proposal = new Proposal(99, 'JEA Company', 'http://portfolio.taniadangelo.com', 'Ruby on Rails', 150, 120, 15, 'test@test.com')
+  proposalThree: Proposal = new Proposal(300, 'EAJ Company', 'http://portfolio.taniadangelo.com', 'Ruby on Rails', 150, 120, 15, 'test@test.com')
+
+  proposals: Proposal[] = [
+    this.proposalOne, 
+    this.proposalTwo, 
+    this.proposalThree
+  ]
+}
