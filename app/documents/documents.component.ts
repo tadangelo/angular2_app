3711b91d6c001b21ee9c5cb186704b6bac21a1bf
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-11 16:44:52 -0600

    Integrated observables and rendered api data

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,37 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.components.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
 
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
-  
-  documents: Document[] = [
-    {
-      title: "My First Doc",
-      description: "test test test",
-      file_url: "http://google.com",
-      updated_at: "12/8/17",
-      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
-    },
-    {
-      title: "My Second Doc",
-      description: "test test test",
-      file_url: "http://google.com",
-      updated_at: "12/8/17",
-      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
-    },
-    {
-      title: "My Last Doc",
-      description: "test test test",
-      file_url: "http://google.com",
-      updated_at: "12/8/17",
-      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
-    }
-  ]
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService,
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000)
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        )
+  }
 }
