94639c537b0e67a49e666c1b3387ce15e7e31a38
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-10 23:16:00 -0600

    Added currency values to proposal input in angular

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -4,8 +4,10 @@ import { Proposal } from './proposal';
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
-  templateUrl: 'proposal-list.component.html'
+  templateUrl: 'proposal-list.component.html',
+  styleUrls: ['proposal-list.component.css']
 })
+
 export class ProposalListComponent {
   proposalOne: Proposal = new Proposal(15, 'AJE Company', 'http://portfolio.taniadangelo.com', 'Ruby on Rails', 150, 120, 15, 'test@test.com')
   proposalTwo: Proposal = new Proposal(99, 'JEA Company', 'http://portfolio.taniadangelo.com', 'Ruby on Rails', 150, 120, 15, 'test@test.com')
