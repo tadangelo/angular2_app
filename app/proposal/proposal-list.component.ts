7170a5c0ec1a1d123b7f96d24ea2086e3f4df3f5
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-11 22:35:33 -0600

    Created custom link function and updated styles

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Proposal } from './proposal';
 import { Observable } from 'rxjs/Rx';
 import { ProposalService } from './proposal.service';
@@ -18,6 +19,7 @@ export class ProposalListComponent implements OnInit {
 
   constructor(
     private proposalService: ProposalService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -31,5 +33,10 @@ export class ProposalListComponent implements OnInit {
           proposals => this.proposals = proposals,
           error => this.errorMessage = <any>error
         )
+  }
+
+  goToShow(proposal: Proposal): void {
+    let link = ['/proposal', proposal.id];
+    this.router.navigate(link);
   }
 }
